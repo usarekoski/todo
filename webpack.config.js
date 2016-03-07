b73ee3e2b0416ea3d6ef13bbf38c92f0d427ffc7
usarekoski <ukonvaaja10@gmail.com>
2016-03-07 15:46:53 +0200

    Add webpack with config file and some loaders

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+
+module.exports = {
+  context: path.join(__dirname, "app"),
+  entry: "./js/app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.min.js",
+  }
+
+};
