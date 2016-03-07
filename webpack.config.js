2a19743212bf9e72cd98f1f8b9170b6e72bffbbf
usarekoski <ukonvaaja10@gmail.com>
2016-03-07 16:02:22 +0200

    Correct build direcotry in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 
 module.exports = {
-  context: path.join(__dirname, "app"),
+  context: path.join(__dirname, "client"),
   entry: "./js/app.js",
   module: {
     loaders: [
@@ -30,7 +30,7 @@ module.exports = {
   },
 
   output: {
-    path: path.join(__dirname, "build"),
+    path: path.join(__dirname, "public"),
     filename: "bundle.min.js",
   }
 
