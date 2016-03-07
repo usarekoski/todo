a8478d0bf7ed084b9ab683b7b5ce7a15c13f7977
usarekoski <ukonvaaja10@gmail.com>
2016-03-07 16:01:35 +0200

    Add app.js and intial layout component

---
+++ client/js/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Layout from "./components/layout";
+
+const App = document.getElementById("app");
+
+ReactDOM.render(
+  <Layout />,
+  App
+);
