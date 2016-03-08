c616f00898227bcdc3d81bf071c1ad2ab81d05d4
usarekoski <ukonvaaja10@gmail.com>
2016-03-08 19:51:15 +0200

    Add todoConstants

---
+++ client/js/constants/todoConstants.js
@@ -0,0 +1,12 @@
+import keyMirror from "keymirror";
+
+const TodoConstants = keyMirror({
+  TODO_CREATE: null,
+  TODO_DELETE: null,
+  TODO_DONE: null,
+  SELECT_ALL: null,
+  SELECT_ACTIVE: null,
+  SELECT_DONE: null
+});
+
+export default TodoConstants
