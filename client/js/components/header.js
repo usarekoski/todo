a546d3925dd624e22bb111b10a7d4728f30a7f21
usarekoski <ukonvaaja10@gmail.com>
2016-03-09 15:00:18 +0200

    Add initial header styling

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "../../css/components/header.scss";
+
 export default class Header extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="header">
         <h1>Todo app</h1>
       </div>
     )
