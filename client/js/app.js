768cd7281267a50daad60040e59a14077e52e388
usarekoski <ukonvaaja10@gmail.com>
2016-03-13 13:06:18 +0200

    Add font-awesome package to project

--- client/js/app.js
+++ client/js/app.js
@@ -5,6 +5,7 @@ import Layout from "./components/layout";
 
 import "../css/normalize.scss";
 import "../css/app.scss";
+import "font-awesome/scss/font-awesome.scss";
 
 const App = document.getElementById("app");
 
